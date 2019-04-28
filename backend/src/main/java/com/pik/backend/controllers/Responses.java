40c7e15e91b6d989812d19e377ef68a513b04779
jferec <janferec@gmail.com>
2019-04-28 18:31:07 +0200

    typo fix

--- backend/src/main/java/com/pik/backend/controllers/Responses.java
+++ backend/src/main/java/com/pik/backend/controllers/Responses.java
@@ -3,27 +3,22 @@ package com.pik.backend.controllers;
 import org.springframework.http.ResponseEntity;
 
 public class Responses {
-    public static ResponseEntity internalError(){
+    public static ResponseEntity internalError() {
         return ResponseEntity
                 .status(500)
                 .build();
     }
 
-    public static ResponseEntity badRequest(String message){
+    public static ResponseEntity badRequest(String message) {
         return ResponseEntity
                 .badRequest()
                 .body(message);
     }
 
-    public static ResponseEntity notFound(){
+    public static ResponseEntity notFound() {
         return ResponseEntity
-                .badRequest()
+                .notFound()
                 .build();
     }
 
-    public static ResponseEntity ok(){
-        return ResponseEntity
-                .ok()
-                .build();
-    }
 }
